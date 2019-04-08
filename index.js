2c83d1dcdecafed41067b4c8e191f42a94c87009
Muhammad Riza <riza.riza69@gmail.com>
2019-04-08 17:16:36 +0700

    Project Javascript Loops

---
+++ index.js
@@ -0,0 +1,36 @@
+"use strict"
+
+
+let iteration = 1
+
+do {
+  console.log(iteration)
+  iteration += 5
+} while (iteration < 100)
+
+// Feel free to have some experiments with comparisons
+
+for (let index = 0; index <= 100; index += 10) {
+  console.log(index)
+}
+
+const numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23]
+
+for (let index = 0; index < numbers.length; index++) {
+  const element = numbers[index]
+  console.log(element)
+}
+
+// Feel free to have some experiments with comparisons
+
+for (let i = 0; i < 5; i++) {
+  console.log('*')
+}
+
+for (let i = 0; i < 5; i++) {
+  let counter = ''
+  for (let j = 0; j < 5; j++) {
+    counter += '*'
+  }
+  console.log(counter)
+}
